31713f551c18fe67d1001b99da589420ae14f103
Nico <70008205+N1Keey@users.noreply.github.com>
2022-03-24 13:39:14 +0000

    made suitable for replit

--- database.py
+++ database.py
@@ -5,34 +5,36 @@ import json
 import random
 import math
 
-engine = create_engine('sqlite:///users.db',connect_args={'check_same_thread': False})
+engine = create_engine('sqlite:///users.db',
+                       connect_args={'check_same_thread': False})
 
-connection=engine.connect()
-Base=declarative_base()
-Session= sessionmaker(bind=engine)
-session=Session()
+connection = engine.connect()
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-roles_users = Table('roles_users',Base.metadata,
+roles_users = Table(
+    'roles_users', Base.metadata,
     Column('user_id', Integer, ForeignKey('users.id', ondelete="CASCADE")),
-    Column('role_id', Integer, ForeignKey('roles.id', ondelete="CASCADE"))
-    )
+    Column('role_id', Integer, ForeignKey('roles.id', ondelete="CASCADE")))
+
 
 class User(Base):
-    __tablename__='users'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(100), unique=True)
     password = Column(String(255))
-    roles = relationship('Role', secondary=roles_users) 
-    
+    roles = relationship('Role', secondary=roles_users)
+
     def regist(user_email, user_password):
         """Adds User to Database and gives the standard-role: 'User'
         Parameter: user_email = String
         Parameter: user_password = String
         """
-        new_User=User(email=user_email, password=user_password)
-        new_user_role=session.query(Role).filter(Role.name=='User').first()
-        new_User.roles=[new_user_role]
+        new_User = User(email=user_email, password=user_password)
+        new_user_role = session.query(Role).filter(Role.name == 'User').first()
+        new_User.roles = [new_user_role]
         session_add_and_commit(new_User)
 
     def login(user_email, user_password):
@@ -41,11 +43,11 @@ class User(Base):
         Parameter: user_email = String
         Parameter: user_password = String
         """
-        login_bool=False
-        users=session.query(User)
+        login_bool = False
+        users = session.query(User)
         for user in users:
-            if user_email==user.email and user_password==user.password:
-                login_bool=True
+            if user_email == user.email and user_password == user.password:
+                login_bool = True
         return login_bool
 
     def getall2Dict():
@@ -53,26 +55,32 @@ class User(Base):
         Dict={'ID':user.id,'Email':user.email,'Password':user.password,'Roles':userrolelist}
         Return: userdicts = List (dict)
         """
-        userdicts=[]
-        users=session.query(User).join(User.roles).all()
+        userdicts = []
+        users = session.query(User).join(User.roles).all()
         for user in users:
-            userrolelist=[]
+            userrolelist = []
             for role in user.roles:
-                userrolelist.append(role.name) 
-            userdict={'ID':user.id,'Email':user.email,'Password':user.password,'Roles':userrolelist}
-            userdicts.append(userdict)  
+                userrolelist.append(role.name)
+            userdict = {
+                'ID': user.id,
+                'Email': user.email,
+                'Password': user.password,
+                'Roles': userrolelist
+            }
+            userdicts.append(userdict)
         return userdicts
 
     def delete(user_ID):
         """Deletes User with given user_ID
         Parameter: user_ID = String"""
-        user=session.query(User).filter(User.id==user_ID)
+        user = session.query(User).filter(User.id == user_ID)
         if user.first().email != 'Nschick@mail.hs-ulm.de':
             user.delete()
             session.commit()
 
+
 class Role(Base):
-    __tablename__='roles'
+    __tablename__ = 'roles'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(40), unique=True)
@@ -82,21 +90,24 @@ class Role(Base):
         """Adds Role to Database
         Parameter: role_name = String, role_description = String
         """
-        new_Role=Role(name=role_name, description=role_description)
+        new_Role = Role(name=role_name, description=role_description)
         session_add_and_commit(new_Role)
 
     def role2User(user_email, role_name):
         """Adds Role with role_name to User with user_email
         Parameter: role_name = String, role_description = String
         """
-        user_roles=[]
-        user=session.query(User).join(User.roles).filter(User.email==user_email).first()
+        user_roles = []
+        user = session.query(User).join(
+            User.roles).filter(User.email == user_email).first()
         for role in user.roles:
             user_roles.append(role)
-        new_user_role=session.query(Role).filter(Role.name==role_name).first()
+        new_user_role = session.query(Role).filter(
+            Role.name == role_name).first()
         user_roles.append(new_user_role)
-        user.roles=user_roles
+        user.roles = user_roles
         session.commit()
+
 
 def session_add_and_commit(new_obj_name):
     """speichert new_obj_name in Datenbank ein
